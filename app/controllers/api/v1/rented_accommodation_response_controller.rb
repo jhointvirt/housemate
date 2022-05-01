8a0166f9ba094ebac9579f358968775509bd9162
Egor-kl <egor.kliutsuk@gmail.com>
2022-05-01 13:07:23 +0300

    feat: add new action with tests

--- app/controllers/api/v1/rented_accommodation_response_controller.rb
+++ app/controllers/api/v1/rented_accommodation_response_controller.rb
@@ -7,4 +7,27 @@ class Api::V1::RentedAccommodationResponseController < ApplicationController
   def show_by_current_user
     render json: current_profile.rented_accommodation_response, status: :ok
   end
+
+  def create
+    @rented_accommodation = RentedAccommodation.find(params[:rented_accommodation_response][:rented_accommodation_id])
+
+    return render json: { message: 'Impossible on your own.' }, status: :bad_request if current_profile.id == @rented_accommodation[:profile_id]
+    return render json: { message: 'You have already responded.' }, status: :bad_request unless RentedAccommodationResponse.find_by(profile_id: current_profile.id).blank?
+
+    @created_rented_accommodation_response = RentedAccommodationResponse.new(rented_accommodation_response_params)
+    @created_rented_accommodation_response.profile_id = current_profile.id
+    @created_rented_accommodation_response.save
+
+    if @created_rented_accommodation_response.errors.empty?
+      render json: @created_rented_accommodation_response, status: :ok
+    else
+      render json: @created_rented_accommodation_response.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def rented_accommodation_response_params
+    params.require(:rented_accommodation_response).permit(:message, :profile_id, :rented_accommodation_id)
+  end
 end
--- spec/requests/api/v1/rented_accommodation_response_spec.rb
+++ spec/requests/api/v1/rented_accommodation_response_spec.rb
@@ -6,23 +6,9 @@ RSpec.describe "Api::V1::RentedAccommodationResponses", type: :request do
     @rented_accommodation_second = generate_rented_accommodation
     @first_user = create_user_with_profile
     @second_user = create_user_with_profile
-    RentedAccommodationResponse.create(
-      message: "First",
-      profile_id: @first_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_first.id
-    )
-
-    RentedAccommodationResponse.create(
-      message: "Second",
-      profile_id: @first_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_second.id
-    )
-
-    RentedAccommodationResponse.create(
-      message: "Third",
-      profile_id: @second_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_second.id
-    )
+    generate_rented_accommodation_response(@first_user.profile.id, @rented_accommodation_first.id)
+    generate_rented_accommodation_response(@first_user.profile.id, @rented_accommodation_first.id)
+    generate_rented_accommodation_response(@second_user.profile.id, @rented_accommodation_first.id)
 
     @all = RentedAccommodationResponse.all
     @headers = { "Authorization": "Bearer " + get_access_token(@first_user) }
@@ -30,6 +16,7 @@ RSpec.describe "Api::V1::RentedAccommodationResponses", type: :request do
     get '/api/v1/rented_accommodation_response', :headers => @headers
     expect(response.status).to eq(200)
     expect(response.body).to eq(@all.to_json)
+    expect(3).to eq(@all.length)
   end
 
   it "returns status 200 when get show by current profile" do
@@ -37,24 +24,10 @@ RSpec.describe "Api::V1::RentedAccommodationResponses", type: :request do
     @rented_accommodation_second = generate_rented_accommodation
     @first_user = create_user_with_profile
     @second_user = create_user_with_profile
-
-    RentedAccommodationResponse.create(
-      message: "First",
-      profile_id: @first_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_first.id
-    )
-
-    RentedAccommodationResponse.create(
-      message: "Second",
-      profile_id: @first_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_second.id
-    )
-
-    RentedAccommodationResponse.create(
-      message: "Third",
-      profile_id: @second_user.profile.id,
-      rented_accommodation_id: @rented_accommodation_second.id
-    )
+    
+    generate_rented_accommodation_response(@first_user.profile.id, @rented_accommodation_first.id)
+    generate_rented_accommodation_response(@first_user.profile.id, @rented_accommodation_first.id)
+    generate_rented_accommodation_response(@second_user.profile.id, @rented_accommodation_first.id)
 
     @all = @first_user.profile.rented_accommodation_response
     @headers = { "Authorization": "Bearer " + get_access_token(@first_user) }
@@ -62,5 +35,66 @@ RSpec.describe "Api::V1::RentedAccommodationResponses", type: :request do
     get '/api/v1/rented_accommodation_response/show_by_current_user', :headers => @headers
     expect(response.status).to eq(200)
     expect(response.body).to eq(@all.to_json)
+    expect(2).to eq(@all.length)
+  end
+
+  it 'returns status 401 when create' do
+    post '/api/v1/rented_accommodation_response/', :headers => nil, :params => { rented_accommodation_response: {} }
+    expect(response.status).to eq(401)
+  end
+
+  it 'returns status 400 when create with message Impossible on your own' do
+    @rented_accommodation = generate_rented_accommodation
+    profile_id = @rented_accommodation.profile_id
+    @user = User.find(Profile.find(profile_id).user_id)
+
+    @headers = { "Authorization": "Bearer " + get_access_token(@user) }
+
+    post '/api/v1/rented_accommodation_response/', :headers => @headers, :params => { rented_accommodation_response: {
+      rented_accommodation_id: @rented_accommodation.id
+    }}
+    expect(response.status).to eq(400)
+    @body = JSON.parse(response.body)
+    expect(@body['message']).to eq('Impossible on your own.')
+  end
+
+  it 'returns status 400 when create with message You have already responded.' do
+    @rented_accommodation = generate_rented_accommodation
+    @user = create_user_with_profile
+
+    @headers = { "Authorization": "Bearer " + get_access_token(@user) }
+
+    post '/api/v1/rented_accommodation_response/', :headers => @headers, :params => { rented_accommodation_response: {
+      rented_accommodation_id: @rented_accommodation.id
+    }}
+    expect(response.status).to eq(200)
+    post '/api/v1/rented_accommodation_response/', :headers => @headers, :params => { rented_accommodation_response: {
+      rented_accommodation_id: @rented_accommodation.id
+    }}
+    expect(response.status).to eq(400)
+    @body = JSON.parse(response.body)
+    expect(@body['message']).to eq('You have already responded.')
+  end
+
+  it 'returns status 200 when create' do
+    @rented_accommodation = generate_rented_accommodation
+    @user = create_user_with_profile
+
+    @headers = { "Authorization": "Bearer " + get_access_token(@user) }
+
+    post '/api/v1/rented_accommodation_response/', :headers => @headers, :params => { rented_accommodation_response: {
+      rented_accommodation_id: @rented_accommodation.id
+    }}
+    expect(response.status).to eq(200)
+  end
+
+  private 
+
+  def generate_rented_accommodation_response(profile_id, rented_accommodation_id)
+    RentedAccommodationResponse.create(
+      message: "First",
+      profile_id: profile_id,
+      rented_accommodation_id: rented_accommodation_id
+    )
   end
 end
